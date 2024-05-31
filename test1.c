b7ecdffeb3d57fc0497977b9789e8d429cfa23ed
ShashimaZoysa <129250242+ShashimaZoysa@users.noreply.github.com>
2024-05-31 12:38:12 +0530

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,11 @@
+function calNewVal (inputVal : integer ) -> integer { localVar constant1 : integer;
+        constant1 = 5;
+        
+        localVar num1 : integer;
+        num1 = 2;
+        
+        localVar result : integer;
+        result = (inputVal * constant1) / constant2 + constant1;
+        
+        return (result);
+    }
